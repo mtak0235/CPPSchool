f20635212689d88509a3d4daa5746b45430db341
mtak <jesustark0235@gmail.com>
2022-09-29 14:10:28 +0900

    data ing

---
+++ second.py
@@ -0,0 +1,50 @@
+class Polynomial:
+    def __init__(self) -> None:
+        self.p = []
+
+    def degree(self):
+        return len(self.p)
+    
+    def read_poly(self):
+        size = int(input('다항식의 최고 차수를 입력하시오:\t'))
+        self.p = [0 for _ in range(size)]
+        for i in range(size - 1, - 1, -1):
+            self.p[i] = int(input('x^' + int(i) + '의 계수:\t'))
+    
+    def evaluate(self, scalar):
+        ret = 0
+        for idx, i in enumerate(self.p):
+            ret += i * (scalar ** idx)
+        return ret
+
+    def add(self, scalar):
+        ret = [0 for _ in range(max(len(scalar, self.p)))]
+        for idx, i in enumerate(self.p):
+            ret[idx] += i
+        for idx, i in enumerate(scalar.p):
+            ret[idx] += i
+        return ret
+    
+    def inverse(self):
+        def inv(a):
+            return a * (-1)
+        self.p = map(inv, self.p)
+    
+    def display(self, s):
+        to_print = ''
+        for i in range(len(self.p), -1, -1):
+            to_print += str(self.p[i]) + 'x^' + str(i)
+        print(s + to_print)
+
+p = Polynomial()
+p.read_poly()
+p.display("A(x) =\t")
+q = Polynomial
+q.read_poly()
+q.display()
+q.display("B(x) =\t")
+print (p.evaluate(3))
+print(p.add(q))
+p.inverse()
+
+
